d43b7c11d09253606e5058f4646c320b73fbddc0
Leandro Emanuel López <lopezlean@gmail.com>
2023-10-17 14:29:45 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), mdx()],
-
+  integrations: [tailwind(), preact(), mdx(), sitemap()],
   site: "https://dermapenmax.com",
   // GitLab Pages requires exposed files to be located in a folder called "public".
   // So we're instructing Astro to put the static build output in a folder of that name.
   outDir: "public",
-
   // The folder name Astro uses for static files (`public`) is already reserved
   // for the build output. So in deviation from the defaults we're using a folder
   // called `static` instead.
-  publicDir: "static",
+  publicDir: "static"
 });
