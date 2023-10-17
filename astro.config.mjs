515512ed95e311bb617b77b81c2e745798d13920
Leandro Emanuel López <lopezlean@gmail.com>
2023-10-17 14:26:34 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,8 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact(), mdx()],
-  buildOptions: {
-    site: "https://dermapenmax.com",
-    // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
+
+  site: "https://dermapenmax.com",
   // GitLab Pages requires exposed files to be located in a folder called "public".
   // So we're instructing Astro to put the static build output in a folder of that name.
   outDir: "public",
