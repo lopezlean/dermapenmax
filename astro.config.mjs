0fac241c13d54f524c935ef21b97cf3f8fa3219d
Leandro Emanuel López <lopezlean@gmail.com>
2023-10-26 09:03:27 +0200

    Remove gitlab

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   integrations: [tailwind(), preact(), mdx(), sitemap()],
   site: "https://dermapenmax.com",
-  // GitLab Pages requires exposed files to be located in a folder called "public".
-  // So we're instructing Astro to put the static build output in a folder of that name.
-  outDir: "public",
-  // The folder name Astro uses for static files (`public`) is already reserved
-  // for the build output. So in deviation from the defaults we're using a folder
-  // called `static` instead.
-  publicDir: "static",
+
   image: {
     service: squooshImageService(),
 
